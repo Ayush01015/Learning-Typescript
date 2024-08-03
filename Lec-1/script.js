534afd136fba9134e539594cdfbe9e88689692ca
Ayush Shrivastav <ayushshrivastav575@gmail.com>
2024-08-03 17:08:45 +0530

    Basic Types In typescript

--- Lec-1/script.js
+++ Lec-1/script.js
@@ -3,67 +3,47 @@
  * Arrays, Tuples
  * Any, Unknown, Never, Void
  */
-
 // let a; avoid giving type any to variables in ts.
-let a1 : number;
+var a1;
 a1 = 12;
 a1 = +"12"; //still a number js folks knows this 
-
 console.log(a1);
-
-
-let arr1 : [];
-let fname : string;
-
+var arr1;
+var fname;
 // ? Tuples
-let array1 : [number,string] = [1,"Ayush"]; // static and fixed size
+var array1 = [1, "Ayush"]; // static and fixed size
 // let array1 : [number,string] = [1,"Ayush",""]; //error
 console.log(array1);
-
-/* 
-    small issue with tuples 
+/*
+    small issue with tuples
     we can still push elements in the array1
 */
-
 array1.push("Ayush Shrivastav");
 // it got pushed it tuple
 console.log(array1);
-
-
 // any default
 // unknown - when we are not sure about type
-
-let variable : unknown = 12;
-variable  = "Some String";
-variable  = true;
-
+var variable = 12;
+variable = "Some String";
+variable = true;
 // Never - we use it when we want to specify that it never ends like infinite loops
-
-function infineRun() : never{
-    while(true){
+function infineRun() {
+    while (true) {
         console.log("infinite");
-        
     }
 }
-
 // fact about never - code after never don't get executed
-
 // VOID - returns nothing
-
-function hello() : void{
+function hello() {
     console.log("hello");
-    
 }
-
 hello();
-
 // Enums
-
-enum Directions{
-    top = "TOP",
-    right = "RIGHT",
-    bottom = "BOTTOM",
-    left = "LEFT"
-}
-
+var Directions;
+(function (Directions) {
+    Directions["top"] = "TOP";
+    Directions["right"] = "RIGHT";
+    Directions["bottom"] = "BOTTOM";
+    Directions["left"] = "LEFT";
+})(Directions || (Directions = {}));
 console.log(Directions.top);
